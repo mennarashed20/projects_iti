a0bbecc784efb1e2b91cf559411fb3ff6aefc66f
mennarashed132000@gmail.com <mennarashed132000@gmail.com>
2024-07-24 18:59:56 +0200

    app1

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,10 +2,10 @@
 List imageList = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZiZTbJRQ0f6nlMCZATPCcNBmd-fRA0UhhPw&s",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE6MJGf5aOBXXbTBPdhJjQPWatzY-i7EYObg&s",
-  "https://png.pngtree.com/element_our/png/20181026/woman-avatar-professional-icon-woman-flat-icon-woman-avatar-woman-character-png_219870.jpg"
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJjlqGUjmSX56wQ0b4BKG7y3G0BeNN9V0z2w&s"
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWliiFZCwh1xp6vkkCL1EENr5O4pwoBJSbWw&s",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSvZy7sxBUxsGdSGUxu7GbRgw3fK_AiA_3TA&s",
-  "https://img.freepik.com/premium-vector/girl-s-face-with-beautiful-smile-female-avatar-website-social-network_499739-527.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPli1sU6Mn7OOA8zlNcJKYvvifiUmolKGt1Q&s",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPli1sU6Mn7OOA8zlNcJKYvvifiUmolKGt1Q&s",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJjlqGUjmSX56wQ0b4BKG7y3G0BeNN9V0z2w&s",
 ];
@@ -15,16 +15,16 @@ List<Map> jsonList = [
 
     "name": "mai",
     "message": "Hi..",
-    "image": imageList[0],
+    "image": imageList[3],
     "time": "6 AM",
     "count": "2"
   },
 
   {
     "count": "3",
-    "name" : "olaa",
+    "name" : "ola",
     "message": "Hello From",
-    "image": imageList[1],
+    "image": imageList[2],
     "time": "9 AM",
 
   },
@@ -33,52 +33,52 @@ List<Map> jsonList = [
 
     "name": "marwa",
     "message": "Hi..",
-    "image": imageList[0],
-    "time": "6 AM",
+    "image": imageList[1],
+    "time": "7 AM",
 
   },
   {
     "count": "7",
     "name": "menna",
     "message": "Hello From",
-    "image": imageList[3],
-    "time": "9 AM",
+    "image": imageList[0],
+    "time": "11 AM",
 
   },
 
   {
     "count": "9",
 
-    "name": "mai",
+    "name": "farida",
     "message": "Hi..",
-    "image": imageList[0],
-    "time": "6 AM",
+    "image": imageList[5],
+    "time": "9 AM",
 
   },
   {
     "count": "6",
 
-    "name": "olaa",
+    "name": "meran",
     "message": "Hello From",
     "image": imageList[1],
-    "time": "9 AM",
+    "time": "12 AM",
 
   },
   {
     "count": "11",
 
-    "name": "marwa",
+    "name": "nour",
     "message": "Hi..",
-    "image": imageList[0],
+    "image": imageList[2],
     "time": "6 AM",
 
   },
   {
     "count": "4",
 
-    "name": "menna",
+    "name": "fatma",
     "message": "Hello From",
-    "image": imageList[3],
+    "image": imageList[4],
     "time": "9 AM",
 
   },
